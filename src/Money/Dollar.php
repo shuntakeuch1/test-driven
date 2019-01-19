698ca9245bf804d7c723a76f5c12b315fffb9992
shuntakeuch1 <takeuchishun89@gmail.com>
2019-01-20 02:20:28 +0900

    明白な実装

---
+++ src/Money/Dollar.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Money;
+
+
+class Dollar
+{
+    public $amount;
+
+    function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier): Dollar
+    {
+        return new Dollar($this->amount * $multiplier);
+    }
+
+}

f420592b8666d6035fe2e57e31ceb8f37d0b4f25
shuntakeuch1 <takeuchishun89@gmail.com>
2019-01-20 17:51:25 +0900

    三角測量

--- src/Money/Dollar.php
+++ src/Money/Dollar.php
@@ -5,16 +5,33 @@ namespace Money;
 
 class Dollar
 {
+    /** @var int  */
     public $amount;
 
+    /**
+     * Dollar constructor.
+     * @param int $amount
+     */
     function __construct(int $amount)
     {
         $this->amount = $amount;
     }
 
+    /**
+     * @param int $multiplier
+     * @return Dollar
+     */
     public function times(int $multiplier): Dollar
     {
         return new Dollar($this->amount * $multiplier);
     }
 
+    /**
+     * @param Dollar $dollar
+     * @return bool
+     */
+    public function equals(Dollar $dollar):bool
+    {
+        return $this->amount === $dollar->amount;
+    }
 }
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -6,12 +6,26 @@ use PHPUnit\Framework\TestCase;
 class MoneyTest extends TestCase
 {
 
-    public function testMultiplication()
+    /**
+     * 乗算
+     * @test
+     */
+    public function Multiplication()
     {
         $five = new Dollar(5);
         $product = $five->times(2);
         $this->assertSame(10, $product->amount);
         $product = $five->times(3);
         $this->assertSame(15, $product->amount);
+    }
+
+    /**
+     * 同等 三角測量
+     * @test
+     */
+    public function Equality()
+    {
+        $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
+        $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
     }
 }
